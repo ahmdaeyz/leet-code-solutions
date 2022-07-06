4e9fd3862134277a512c361942fcdfe77b6ff6e0
Ahmed Aboelyazeed <ahmdaeyz@gmail.com>
2022-07-06 21:46:02 +0200

    Time: 316 ms (5.01%), Space: 33.7 MB (35.31%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun fib(n: Int): Int {
+        if(n == 0){
+            return 0
+        }else if(n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2)
+    }
+}
