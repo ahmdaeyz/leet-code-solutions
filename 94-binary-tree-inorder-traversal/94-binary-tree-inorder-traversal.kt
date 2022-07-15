0e6cae60df807b9765ff391c2747b1607a5b0311
Ahmed Aboelyazeed <ahmdaeyz@gmail.com>
2022-07-15 04:00:06 +0200

    Time: 200 ms (78.13%), Space: 35.7 MB (24.32%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.kt
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.kt
@@ -9,8 +9,25 @@
  * }
  */
 class Solution {
+    // fun inorderTraversal(root: TreeNode?): List<Int> {
+    //     if(root==null) return listOf()
+    //     return inorderTraversal(root.left) + root.`val` + inorderTraversal(root.right)
+    // }
+    
     fun inorderTraversal(root: TreeNode?): List<Int> {
-        if(root==null) return listOf()
-        return inorderTraversal(root.left) + root.`val` + inorderTraversal(root.right)
+        if (root == null) return listOf()
+        val stack = Stack<TreeNode>()
+        val solution = mutableListOf<Int>()
+        var currentNode = root
+        while (currentNode != null || stack.isNotEmpty()) {
+            while (currentNode != null) {
+                stack.push(currentNode)
+                currentNode = currentNode.left
+            }
+            currentNode = stack.pop()
+            solution.add(currentNode.`val`)
+            currentNode = currentNode.right
+        }
+        return solution
     }
 }
