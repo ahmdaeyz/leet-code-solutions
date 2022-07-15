e48a6654750e5cbe39c4f56d528371cdfc5d3d7f
Ahmed Aboelyazeed <ahmdaeyz@gmail.com>
2022-07-15 03:16:45 +0200

    Time: 286 ms (19.66%), Space: 35.9 MB (12.53%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.kt
@@ -0,0 +1,16 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        if(root==null) return listOf()
+        return inorderTraversal(root.left) + root.`val` + inorderTraversal(root.right)
+    }
+}
