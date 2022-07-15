ac39d710ea42bdb07611718524eaae089f2a966d
Ahmed Aboelyazeed <ahmdaeyz@gmail.com>
2022-07-15 20:37:23 +0200

    Time: 250 ms (75.79%), Space: 37.4 MB (58.96%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.kt
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.kt
@@ -12,7 +12,19 @@ import kotlin.math.max
  */
 class Solution {
     fun maxDepth(root: TreeNode?): Int {
-    if(root == null) return  0
-    return 1 + max(maxDepth(root.left), maxDepth(root.right))
-}
+        if (root == null) return 0
+        val queue = ArrayDeque<TreeNode>();
+        queue.addLast(root)
+        var depth = 0
+        while (queue.isNotEmpty()) {
+            val size = queue.size
+            for (i in 0..size - 1) {
+                val current = queue.removeFirst()
+                if (current.left != null) queue.addLast(current.left!!)
+                if (current.right != null) queue.addLast(current.right!!)
+            }
+            depth++
+        }
+        return depth
+    }
 }
