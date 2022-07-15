d55ed3103be95f177e07e90bfb09d041b3be8568
Ahmed Aboelyazeed <ahmdaeyz@gmail.com>
2022-07-15 03:06:48 +0200

    Time: 203 ms (77.59%), Space: 35.9 MB (11.20%) - LeetHub

---
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.kt
@@ -0,0 +1,16 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun preorderTraversal(root: TreeNode?): List<Int> {
+        if (root == null) return listOf()
+        return listOf(root.`val`) + preorderTraversal(root.left) + preorderTraversal(root.right)
+    }
+}
